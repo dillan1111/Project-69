dbeffa0fedd4bb8a526ca0e272de698802d30bea
dillan1111 <70030386+dillan1111@users.noreply.github.com>
2021-02-20 14:18:09 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+
+import ScanerScreen from "./screens/ScanScreen";
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+      <AppContainer />
+
+
+    );
+    
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Scan: { screen: ScanerScreen }
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
